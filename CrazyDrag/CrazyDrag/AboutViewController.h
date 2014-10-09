4e1390d941b737413faa0339f501052fa8e91e35
lizhiyi [李智奕] <lizhiyi@xiaomi.com>
2014-10-09 23:20:23 +0800

    yes
    yes

---
+++ CrazyDrag/CrazyDrag/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  CrazyDrag
+//
+//  Created by 李智奕 on 14-10-9.
+//  Copyright (c) 2014年 ioslearning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
